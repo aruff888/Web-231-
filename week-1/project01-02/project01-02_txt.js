52878ea07c203e967d1425327c935ea74407dfda
Amanda <aruff@my365.bellevue.edu>
2025-08-20 20:05:04 -0600

    add week 1,2,3,4,5,6,7,8,9 files and project files

---
+++ week-1/project01-02/project01-02_txt.js
@@ -0,0 +1,11 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Amanda Ruff
+    Date:   08/13/25
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
