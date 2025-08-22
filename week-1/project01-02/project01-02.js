d71e5e077fcb2427a36bd831c07352f5240ebb7e
Amanda <aruff@my365.bellevue.edu>
2025-08-21 20:35:54 -0600

    add changes to week-1/project01-02/project01-02.js add week-2/chapter/js02.html add week-2/chapter/js02.js

--- week-1/project01-02/project01-02.js
+++ week-1/project01-02/project01-02.js
@@ -9,4 +9,11 @@
 */
 
 //define variables for service name and service speed
-service1Name="Basic", service2Name="Express", service3Name="Extreme", service4Name="Ultimate", service1Speed="0 Mbps", service2Speed="100 Mbps", service3Speed="500 Mbps", service4Speed="1 Gig";
+service1Name="Basic",
+ service2Name="Express", 
+ service3Name="Extreme", 
+ service4Name="Ultimate", 
+ service1Speed="0 Mbps", 
+ service2Speed="100 Mbps", 
+ service3Speed="500 Mbps", 
+ service4Speed="1 Gig";
